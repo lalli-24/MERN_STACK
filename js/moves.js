60f9ff668ff1dadc1d35ece24f69d00dae3033b7
lalli-24 <sujilalli993@gmail.com>
2026-07-17 15:26:05 +0530

    initial

---
+++ js/moves.js
@@ -0,0 +1,43 @@
+const box=document.getElementById("box");
+let x=0;
+let y=0;
+function moveleft(){
+    x-=20;
+    box.style.left=x+"px";
+}
+function moveright(){
+    x+=20;
+    box.style.left=x+"px";
+}
+function moveup(){
+    y-=20;
+    box.style.top=y+"px";
+}
+function movedown(){
+    y+=20;
+    box.style.top=y+"px";
+}
+
+document.addEventListener(
+    "keydown",
+    (event) => {
+        if (event.key == "ArrowUp") {
+            y -= 30;
+        }
+        if (event.key == "ArrowDown") {
+            y += 30;
+        }
+        if (event.key == "ArrowLeft") {
+            x -= 30;
+        }
+        if (event.key == "ArrowRight") {
+            x += 30;
+        }
+
+        box.style.left = x + "px";
+        box.style.top = y + "px";
+    }
+);
+window.addEventListener("resize", () => {
+    console.log(window.innerWidth);
+});
